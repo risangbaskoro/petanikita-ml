45ddffade5b04a1ba60781a3e5e91f730ca56fee
Risang Baskoro <risangbaskoro@gmail.com>
2023-06-07 12:18:20 +0000

    --wip-- [skip ci]

--- src/convert.py
+++ src/convert.py
@@ -1,5 +1,7 @@
 import os
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 from absl import app
 from absl import flags
 
@@ -10,9 +12,18 @@ import tensorflow as tf
 
 def define_flags():
     flags.DEFINE_string(
-        "model_path", "model/rice_leaf_disease_classifier", "The path to the model."
-    )
+        "model_path", "model/rldc_mobilenet_v1_1_default_1", "The path to the model."
+    )  # TODO: Remove defaults
     flags.DEFINE_string("export_path", "converted_model", "The path to the model.")
+
+def representative_dataset_gen():
+    img_path = "/mnt/disks/persist/rice-leafs-1000px/train/Healthy"
+    img_name = os.listdir(img_path)[0]
+    img = tf.keras.utils.load_img(os.path.join(img_path, img_name), target_size=(224, 224))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    img_array = tf.cast(img_array, tf.float32) / 255.0
+    yield [img_array]
 
 
 def main(_):
@@ -20,6 +31,9 @@ def main(_):
     model_basename = os.path.basename(model_path)
 
     converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.representative_dataset = representative_dataset_gen
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
 
     tflite_model = converter.convert()
 
@@ -28,6 +42,7 @@ def main(_):
 
     with open(os.path.join(FLAGS.export_path, model_basename + ".tflite"), "wb") as f:
         f.write(tflite_model)
+    print("Model successfully converted to TFLite.")
 
 
 if __name__ == "__main__":
--- src/train.py
+++ src/train.py
@@ -17,21 +17,25 @@ FLAGS = flags.FLAGS
 
 def define_flags():
     flags.DEFINE_string(
-        "dataset_path", "/mnt/disks/persist/RiceLeafs/train", "The path to the dataset."
-    )# TODO: REMOVE DEFAULTS to None
-    flags.DEFINE_string("model_export_name", "rice_leaf_disease_classifier", "The name of the model.") # TODO: REMOVE DEFAULTS to None
-    flags.DEFINE_string("model_export_path", "model", "The path to the model.") # TODO: REMOVE DEFAULTS to None
-    flags.DEFINE_bool("augment_data", True, "Whether to augment the data.")
+        "dataset_path",
+        "/mnt/disks/persist/rice-leafs-1000px/train",
+        "The path to the dataset.",
+    )  # TODO: REMOVE DEFAULTS to None
+    flags.DEFINE_string("export_path", None, "Path to export the model")
+    flags.DEFINE_bool("augment_data", False, "Whether to augment the data.")
     flags.DEFINE_integer("batch_size", 200, "The batch size.")
-    flags.DEFINE_string("tpu_address", "local", "The address of the TPU to connect to.") # TODO: REMOVE DEFAULTS to ""
+    flags.DEFINE_string(
+        "tpu", "local", "The address of the TPU to connect to."
+    )  # TODO: REMOVE DEFAULTS to ""
     flags.DEFINE_integer("num_epochs", 10, "The number of epochs to train for.")
 
     flags.mark_flag_as_required("dataset_path")
-    flags.mark_flag_as_required("model_export_name")
-    flags.mark_flag_as_required("model_export_path")
+    flags.mark_flag_as_required("export_path")
 
 
-def get_image_dataset_from_directory(directory, image_size=(224, 224), validation_split=0.2, seed=42): # TODO: Default value can be removed, use flags instead
+def get_image_dataset_from_directory(
+    directory, image_size=(224, 224), validation_split=0.2, seed=42
+):  # TODO: Default value can be removed, use flags instead
     train_ds = tf.keras.utils.image_dataset_from_directory(
         directory,
         image_size=image_size,
@@ -62,15 +66,20 @@ def augment_data(train_ds, val_ds):
         val_ds = val_ds.map(lambda x, y: (augment_layer(x), y))
     return train_ds, val_ds
 
+
 def get_callbacks():
-    callbacks = []
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(
+            monitor="val_accuracy", patience=5, restore_best_weights=True
+        )
+    ]
     return callbacks
 
 
 def main(_):
     tf.keras.backend.clear_session()
 
-    _, strategy = connect_to_tpu(tpu_address=FLAGS.tpu_address)
+    _, strategy = connect_to_tpu(tpu_address=FLAGS.tpu)
 
     train_ds, val_ds = get_image_dataset_from_directory(FLAGS.dataset_path)
 
@@ -86,11 +95,15 @@ def main(_):
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE).repeat()
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE).repeat()
 
+    model_basename = os.path.basename(FLAGS.export_path)
+
     with strategy.scope():
-        model = LeafDiseaseClassifier(num_classes=num_classes, model="MobileNetV2", weights="imagenet") # TODO: Default value can be removed, use flags
+        model = LeafDiseaseClassifier(
+            num_classes=num_classes, name=model_basename[1]
+        )  # TODO: Default value can be removed, use flags
         model.compile(
             optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
             metrics=["accuracy"],
         )
 
@@ -100,13 +113,13 @@ def main(_):
             epochs=FLAGS.num_epochs,
             batch_size=batch_size,
             validation_data=val_ds,
-            validation_steps=10000 // batch_size, # TODO: Default value can be removed, use flags
-            steps_per_epoch=40000 // batch_size, # TODO: Default value can be removed, use flags
+            validation_steps=10000 // batch_size,  # TODO: Default value can be removed, use flags
+            steps_per_epoch=40000 // batch_size,  # TODO: Default value can be removed, use flags
             callbacks=get_callbacks(),
         )
 
-    if FLAGS.model_export_path is not None:
-        model_path = os.path.join(FLAGS.model_export_path, FLAGS.model_export_name)
+    if FLAGS.export_path is not None:
+        model_path = FLAGS.export_path
         model.save(model_path)
 
 
